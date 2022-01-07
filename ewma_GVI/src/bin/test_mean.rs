7d25659c7c8a97138b0482e17adbd6661bf12e45
rickardbrannvall <rickard.brannvall@gmail.com>
2022-01-07 16:06:19 +0000

    boot after mean

--- ewma_GVI/src/bin/test_mean.rs
+++ ewma_GVI/src/bin/test_mean.rs
@@ -5,6 +5,7 @@ use std::env;
 pub struct ConcreteContext {
     enc_full: Encoder,
     enc_half: Encoder,
+    enc_2by3: Encoder,
     bsk: LWEBSK,
     sk: LWESecretKey
 }
@@ -12,32 +13,52 @@ pub struct ConcreteContext {
 impl ConcreteContext {
     pub fn mean_of_pair(&mut self, x1: &VectorLWE, x2: &VectorLWE) -> VectorLWE {
         let term1 = (*x1).bootstrap_nth_with_function(&self.bsk, |x| 0.5 * x, &self.enc_half, 0).unwrap();
-        //println!("term1* {:?}", term1.decrypt_decode(&sk0).unwrap());
-        //term1.pp();
-
         let term2 = (*x2).bootstrap_nth_with_function(&self.bsk, |x| 0.5 * x, &self.enc_half, 0).unwrap();
-        //println!("term2* {:?}", term2.decrypt_decode(&sk0).unwrap());
-        //term2.pp();
-
         let res = term1.add_with_padding(&term2).unwrap();
-        //println!("ewma* {:?}", m1.decrypt_decode(&sk0).unwrap());
-        //m1.pp();   
         return res;
     }  
+
+    pub fn wavg_of_pair(&mut self, x1: &VectorLWE, x2: &VectorLWE, phi: f64) -> VectorLWE {
+        // assert phi is in range [1/3,2/3] 
+        let term1 = (*x1).bootstrap_nth_with_function(&self.bsk, |x| phi * x, &self.enc_2by3, 0).unwrap();
+        let term2 = (*x2).bootstrap_nth_with_function(&self.bsk, |x| (1.-phi) * x, &self.enc_2by3, 0).unwrap();
+        let res = term1.add_with_padding(&term2).unwrap();
+        return res;
+    }    
+    
+    pub fn mean_recursion(&mut self, x: &[VectorLWE]) -> VectorLWE{
+        let n = x.len();
+        let m = n/2;
+        if n == 1 {
+            println!("n, m: {}, {}", n, m);
+            return x[0].clone();
+        }
+        let x_i = self.mean_recursion(&x[..m]);
+        let x_j = self.mean_recursion(&x[m..]);
+        if 2*m == n {
+            println!("n, m: {}, {}", n, m);
+            return self.mean_of_pair(&x_i, &x_j);
+        }
+        else {
+            let phi: f64 = (m as f64)/(n as f64);
+            println!("n, m, phi: {}, {}, {}", n, m, phi);
+            return self.wavg_of_pair(&x_i, &x_j, phi);
+        }
+    }
     
     pub fn mean_of_many(&mut self, x: &[VectorLWE]) -> VectorLWE{
         let n = x.len();
         let m = n/2;
-        println!("n,m: {},{}", n, m);
-        if n > 1 {
-            let x_i = self.mean_of_many(&x[..m]);
-            let x_j = self.mean_of_many(&x[m..]);
-            return self.mean_of_pair(&x_i, &x_j);
-        }
-        else {
+        if n == 1 {
             return x[0].clone();
         }
+        let mut res = self.mean_recursion(&x);
+        if 2*m != n { // in case encoding is not back at full
+            res = res.bootstrap_nth_with_function(&self.bsk, |x| x, &self.enc_full, 0).unwrap(); 
+        }
+        return res;           
     }
+    
 }
 
 
@@ -65,10 +86,13 @@ fn main() -> Result<(), CryptoAPIError> {
     let mut context = ConcreteContext{
         enc_full: Encoder::new(0., 200., 3, 2).unwrap(),
         enc_half: Encoder::new(0., 100., 3, 2).unwrap(),
+        enc_2by3: Encoder::new(0., 135., 3, 2).unwrap(),
         bsk: LWEBSK::load(&bsk00_path),
         sk: LWESecretKey::load(&sk0_LWE_path).unwrap()    
     };    
         
+    /* 
+    // test mean_of_pair
     let x1: Vec<f64> = vec![160.0]; // initial value for moving average process
     println!("x1: {:?}\n", x1);
     
@@ -88,6 +112,7 @@ fn main() -> Result<(), CryptoAPIError> {
     c3.pp();   
     
     println!("res: {}", 0.5*&x1[0]+0.5*&x2[0]);
+    */
 
     /*
     2021-01-01 12:18:40,189.0
@@ -101,23 +126,16 @@ fn main() -> Result<(), CryptoAPIError> {
     2021-01-01 12:58:39,195.0
     2021-01-01 13:03:39,191.0
     */
+
+    // test mean_of_many
     
-    //let xv: Vec<f64> = vec![189.0,195.0,193.0,199.0,193.0,195.0,198.0,202.0,195.0,191.0];
-    let xv: Vec<f64> = vec![189.0,195.0,193.0,199.0,193.0,195.0,198.0,202.0];
+    let xv: Vec<f64> = vec![189.0,195.0,193.0,199.0,193.0,195.0,198.0,202.0,195.0,191.0];
     let n = xv.len();
     
     
     let cv = (0..n).map(|i| VectorLWE::encode_encrypt(&context.sk, &[xv[i]], &context.enc_full).unwrap())
                     .collect::<Vec<_>>();
-    
-    /*
-    let cv = VectorLWE::encode_encrypt(&context.sk, &xv, &context.enc_full).unwrap();
-    let mut cv = vec![c1.clone(); n];
-    for i in 0..n {
-        cv[i] = x.extract_nth(i).unwrap();
-    }
-    */
-    
+        
     let avg = context.mean_of_many(&cv);
     println!("avg* {:?}", avg.decrypt_decode(&context.sk).unwrap());
     avg.pp(); 
