308614604b4202e41cfd0e491c9871789dc5796b
rickardbrannvall <rickard.brannvall@gmail.com>
2022-01-09 18:38:14 +0000

    tidy up functions

--- ewma_GVI/src/bin/test_mean.rs
+++ ewma_GVI/src/bin/test_mean.rs
@@ -73,42 +73,29 @@ fn main() -> Result<(), CryptoAPIError> {
         sk: LWESecretKey::load(&sk0_LWE_path).unwrap()    
     };    
         
-    /* 
     // test mean_of_pair
+    
     let x1: Vec<f64> = vec![160.0]; // initial value for moving average process
     println!("x1: {:?}\n", x1);
     
     let x2: Vec<f64> = vec![140.0]; // initial value for data generating process
     println!("x2: {:?}\n", x2);
 
-    let c1 = VectorLWE::encode_encrypt(&context.sk, &x1, &context.enc_full)?;  
+    let c1 = VectorLWE::encode_encrypt(&context.sk, &x1, &enc)?;  
     println!("x1* {:?}", c1.decrypt_decode(&context.sk).unwrap());
     c1.pp();
 
-    let c2 = VectorLWE::encode_encrypt(&context.sk, &x2, &context.enc_full)?;  
+    let c2 = VectorLWE::encode_encrypt(&context.sk, &x2, &enc)?;  
     println!("x2* {:?}",c2.decrypt_decode(&context.sk).unwrap());
     c2.pp();  
         
-    let c3 = context.mean_of_pair(&c1, &c2, 0.5);
+    let c3 = context.mean_of_pair(&c1, &c2, 0.5, &enc);
     println!("res* {:?}", c3.decrypt_decode(&context.sk).unwrap());
     c3.pp();   
     
     println!("res: {}", 0.5*&x1[0]+0.5*&x2[0]);
-    */
 
-    /*
-    2021-01-01 12:18:40,189.0
-    2021-01-01 12:23:39,195.0
-    2021-01-01 12:28:40,193.0
-    2021-01-01 12:33:39,199.0
-    2021-01-01 12:38:39,193.0
-    2021-01-01 12:43:39,195.0
-    2021-01-01 12:48:39,198.0
-    2021-01-01 12:53:39,202.0
-    2021-01-01 12:58:39,195.0
-    2021-01-01 13:03:39,191.0
-    */
-
+    
     // test mean_of_many
     
     let xv: Vec<f64> = vec![179.0,175.0,183.0,189.0,183.0,185.0,188.0,182.0,175.0,171.0];
@@ -119,34 +106,8 @@ fn main() -> Result<(), CryptoAPIError> {
                     ).collect::<Vec<_>>();
     println!("cv[0]* {:?}", cv[0].decrypt_decode(&context.sk).unwrap());
     cv[0].pp(); 
-
-    let enc100 = Encoder::new(0., 100., 3, 2).unwrap();
-    let enc10 = Encoder::new(0., 10., 3, 2).unwrap();
-    let enc5 = Encoder::new(0., 5., 3, 2).unwrap();
-        
-    let score_IR = |x| if (x >= 70.) && (x <= 180.) {100.0} else {0.0};
-    let ir = (0..n).map(
-                        |i| cv[i].bootstrap_nth_with_function(&context.bsk, score_IR, &enc100, 0).unwrap()
-                    ).collect::<Vec<_>>();
-    println!("ir[0]* {:?}", ir[0].decrypt_decode(&context.sk).unwrap());
-    ir[0].pp(); 
-
-    let score_70 = |x| if x < 70. {100.0} else {0.0};
-    let h70 = (0..n).map(
-                        |i| cv[i].bootstrap_nth_with_function(&context.bsk, score_70, &enc100, 0).unwrap()
-                    ).collect::<Vec<_>>();
-    println!("h70[0]* {:?}", h70[0].decrypt_decode(&context.sk).unwrap());
-    h70[0].pp(); 
     
-    let score_54 = |x| if x < 54. {100.0} else {0.0};
-    let h54 = (0..n).map(
-                        |i| cv[i].bootstrap_nth_with_function(&context.bsk, score_54, &enc100, 0).unwrap()
-                    ).collect::<Vec<_>>();
-    println!("h54[0]* {:?}", h54[0].decrypt_decode(&context.sk).unwrap());
-    h54[0].pp(); 
-    
-    //let avg = context.mean_of_many(&cv, &enc);
-    let avg = context.mean_of_many(&ir, &enc);
+    let avg = context.mean_of_many(&cv, &enc);
     println!("avg* {:?}", avg.decrypt_decode(&context.sk).unwrap());
     avg.pp(); 
     
